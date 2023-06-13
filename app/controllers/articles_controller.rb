4a7a52f33f1274aa0f1b54f0a81dbf2f6d671330
Safa <safaknm99@gmail.com>
2023-06-13 15:34:38 +0500

    Refactored code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
         flash[:notice] = "Article was created successfully."
         redirect_to article_path(@article)
@@ -30,7 +30,7 @@ class ArticlesController < ApplicationController
 
     def update
         #@article= Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] ="Article was updated successfuly"
             redirect_to @article
         else
@@ -48,5 +48,9 @@ class ArticlesController < ApplicationController
     private
     def set_article
         @article=Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 end
